7fa14f4e0361007705c7f44539769863006781c1
Kaseke <kasymkhankhubievnis@gmail.com>
2022-10-23 19:49:39 +0700

    commit changes

--- task1/MainPanel.py
+++ task1/MainPanel.py
@@ -16,8 +16,19 @@ class MainPanel:
         self.win = window
         self.graph_canvas = tk.Canvas(self.win, height=700, width=700, bg='white')
         self.panel_canvas = tk.Canvas(self.win, height=700, width=300)
+        self.menubar = tk.Menu(self.win,)
+
+    def _help(self):
+        pass
+
+    def _calculate(self):
+        pass
 
     def draw_window(self) -> None:
+
+        # filemenu =
+        self.menubar.add_command(label='Справка', command=self._help, font=('Arial', 20))
+        self.win.config(menu=self.menubar)
 
         self.graph_canvas.grid(row=0, column=0, padx=5, pady=5)
         self.panel_canvas.grid(row=0, column=1, pady=5, padx=5)
@@ -26,26 +37,24 @@ class MainPanel:
 
         font_size = 20
 
-        tk.Label(self.panel_canvas, text='Эффективная масса', font=('Arial', font_size)).grid(row=0, column=0)
+        tk.Label(self.panel_canvas, text='Эффективная масса m*', font=('Arial', font_size)).grid(row=0, column=0)
         self.effective_mass = tk.Spinbox(self.panel_canvas, width=7, font=font_size)
         self.effective_mass.grid(row=0, column=1)
 
-        tk.Label(self.panel_canvas, text='Ширина ямы', font=('Arial', font_size)).grid(row=1, column=0)
+        tk.Label(self.panel_canvas, text='Ширина ямы a', font=('Arial', font_size)).grid(row=1, column=0)
         self.hole_width = tk.Spinbox(self.panel_canvas, width=7, font=font_size)
         self.hole_width.grid(row=1, column=1)
 
-        tk.Label(self.panel_canvas, text='Глубина ямы', font=('Arial', font_size)).grid(row=2, column=0)
+        tk.Label(self.panel_canvas, text='Глубина ямы U0', font=('Arial', font_size)).grid(row=2, column=0)
         self.hole_depth = tk.Spinbox(self.panel_canvas, width=7, font=font_size)
         self.hole_depth.grid(row=2, column=1)
 
-        tk.Label(self.panel_canvas, text='Расстояние между ямами', font=('Arial', font_size)).grid(row=3, column=0)
+        tk.Label(self.panel_canvas, text='Расстояние между ямами b', font=('Arial', font_size)).grid(row=3, column=0)
         self.holes_dist = tk.Spinbox(self.panel_canvas, width=7, font=font_size)
         self.holes_dist.grid(row=3, column=1)
 
-        tk.Label(self.panel_canvas, text='Уровень энергии', font=('Arial', font_size)).grid(row=4, column=0)
+        tk.Label(self.panel_canvas, text='Уровень энергии E(q)', font=('Arial', font_size)).grid(row=4, column=0)
         self.energy_level = tk.Label(self.panel_canvas, text='', font=('Arial', font_size), width=7)
         self.energy_level.grid(row=5, column=1)
 
-
-
-
+        tk.Button(self.panel_canvas, text='Расчитать', font=('Arial', 20), command=self._calculate)
