1c5166b53f80953fd67b675246d06cb9f6a2cfb8
Kaseke <kasymkhankhubievnis@gmail.com>
2022-10-23 19:08:13 +0700

    commit changes

--- task1/MainPanel.py
+++ task1/MainPanel.py
@@ -6,14 +6,27 @@ class MainPanel:
     """
 
     """
+    def _draw_work_panel(self):
+        pass
+
+    def _draw_graph(self):
+        pass
 
     def __init__(self, window: tk.Tk):
         self.win = window
-        self.canvas = tk.Canvas(self.win, height=700, width=700, bg='white')
+        self.graph_canvas = tk.Canvas(self.win, height=700, width=700, bg='white')
+        self.panel_canvas = tk.Canvas(self.win, height=700, width=300)
 
     def draw_window(self) -> None:
 
-        self.canvas.place(x=10, y=5)
+        self.graph_canvas.grid(row=0, column=0, padx=5, pady=5)
+        self.panel_canvas.grid(row=0, column=1, pady=5, padx=5)
+
+        # ttk.Separator(self.win, orient='horizontal').grid(row=1, column=0)  # place(x=720, y=725)
+
+        # self.effective_mass = tk.Spinbox(self.win, width=7)
+        # self.effective_mass.place(x=760, y=50)
+
 
 
 
