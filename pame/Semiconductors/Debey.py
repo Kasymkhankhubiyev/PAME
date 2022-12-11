cfc0465d8cb7ad03695651a615174c08485a185d
Kaseke <kasymkhankhubievnis@gmail.com>
2022-12-11 17:47:30 +0700

    added a debey_length

---
+++ pame/Semiconductors/Debey.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def debey_length(epsilon: float, n: float, t: float) -> float:
+    """
+
+    :param epsilon: dielectric constant
+    :param n: carriers concentration
+    :param t: temperature in Kelvin
+    :return: Debey length in cm
+    """
+    k = 1.381e-16  # arg/K
+    e = 1  # eV
+
+    return np.sqrt(epsilon * k * 6.24e11 * t / (4 * np.pi * e ** 2 * n))
