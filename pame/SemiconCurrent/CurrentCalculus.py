36c8efbca1cd5cd6e3e371b5abe6bf491e995844
Kaseke <kasymkhankhubievnis@gmail.com>
2022-12-13 22:07:30 +0700

    some refactoring and fixing

--- pame/SemiconCurrent/CurrentCalculus.py
+++ pame/SemiconCurrent/CurrentCalculus.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from typing import NamedTuple
 from pame.ChargedParticlesInSemicondactor.CalculateParticles import calc_Nv, calc_Nc, calc_n, calc_p
-from pame.constants import k
+from pame.constants import k, e
 
 me_effective = float
 mh_effective = float
@@ -16,11 +16,28 @@ class Current(NamedTuple):
 
 
 def count_p_n(ni2: float, nc: float, Ef_n: float, Eg: float, t: float) -> float:
-    return ni2 / (nc * np.exp(Ef_n-Eg)/(k * t))
+    """
+    :math: $p_n0 = \frac{n_i^2}{N_cexp(\frac{E_f_n-E_g}{kT})}$
+    :param ni2: electrons concentration in non-degenerate semiconductor
+    :param nc: electrons density
+    :param Ef_n: fermi energy in n-type in eV
+    :param Eg: energy gap in eV
+    :param t: temperature in Kelvin
+    :return: concentration of protons in n-area
+    """
+    return ni2 / (nc * np.exp((Ef_n-Eg)/(k * t)))
 
 
 def count_n_p(ni2: float, nv: float, Ef_p: float, t: float) -> float:
-    return ni2 / (nv * np.exp(-Ef_p)/(k * t))
+    """
+    :math: $n_p0 = \frac{n_i^2}{N_vexp(\frac{-E_f_p}{kT})}$
+    :param ni2: electrons concentration in non-degenerate semiconductor
+    :param nv: protons density
+    :param Ef_p: fermi energy in p-type in eV
+    :param t: temperature in eV
+    :return: concentration of electrons in p-area
+    """
+    return ni2 / (nv * np.exp((-Ef_p)/(k * t)))
 
 
 def count_ni(t: float, me: float, mh: float, Eg: float) -> float:
@@ -34,48 +51,45 @@ def count_ni(t: float, me: float, mh: float, Eg: float) -> float:
 def count_pi(t: float, me: float, mh: float, Eg: float) -> float:
     Nc = calc_Nc(me=me, t=t)
     Nv = calc_Nv(mh=mh, t=t)
-    # k = 1.38e-16  # эрг/К
     return (Nc * Nv * np.exp(-1. * Eg / (2 * k * t)))**0.5  # 10^3
 
 
-def _count_Jp(me: float, mh: float, Na: float, Eg: float, Ef_n: float, t: float, Dp: float, Lp: float) -> float:
-    e = 1.6e-19  # Кулон
-    # Dp = 12  # cm^2/s
-    lp = Lp  # 1e-3  # cm
-    nn = Na
-
-    pi = count_pi(t=t, me=me, mh=mh, Eg=Eg)
-
-    # pn0 = pi**2 / nn
-
-    pn0 = count_p_n(ni2=pi**2, nc=calc_Nc(me=me, t=t), Ef_n=Ef_n, Eg=Eg, t=t)
-
-    Jp = (e * Dp * pn0) / lp
-
-    return Jp
+def _count_Jp(me: float, mh: float, Eg: float, Ef_n: float, t: float, Dp: float, Lp: float) -> float:
+    """
+    :param me: effective mass of electron
+    :param mh: effective mass of proton
+    :param Eg: energy gap in eV
+    :param Ef_n: fermi energy of n-type in eV
+    :param t: temperature in Kelvin
+    :param Dp: protons diffusion coefficient
+    :param Lp: protons diffusion length
+    :return: current density
+    """
+    pn0 = count_p_n(ni2=count_pi(t=t, me=me, mh=mh, Eg=Eg)**2, nc=calc_Nc(me=me, t=t), Ef_n=Ef_n, Eg=Eg, t=t)
+    return (e * Dp * pn0) / Lp
 
 
-def _count_Jn(me: float, mh: float, Nd: float, Eg: float, Ef_p: float, t: float, Dn: float, Ln: float) -> float:
-    e = 1.6e-19  # Кулон
-    # Dn = Dn  # 36  # cm^2/s
-    ln = Ln  # 5e-3  # cm
-    pp = Nd
+def _count_Jn(me: float, mh: float, Eg: float, Ef_p: float, t: float, Dn: float, Ln: float) -> float:
+    """
+    :param me: effective mass of electron
+    :param mh: effective mass of proton
+    :param Eg: energy gap in eV
+    :param Ef_p: fermi energy for p-type in eV
+    :param t: temperature in Kelvin
+    :param Dn: electrons diffusion coefficient
+    :param Ln: electrons diffusion length
+    :return: current density
+    """
+    np0 = count_n_p(ni2=count_ni(t=t, me=me, mh=mh, Eg=Eg)**2, nv=calc_Nv(mh=mh, t=t), Ef_p=Ef_p, t=t)
 
-    ni = count_ni(t=t, me=me, mh=mh, Eg=Eg)
-    # np0 = ni**2 / pp
-
-    np0 = count_n_p(ni2=ni**2, nv=calc_Nv(mh=mh, t=t), Ef_p=Ef_p, t=t)
-
-    Jn = ((e * Dn * np0) / ln)
-
-    return Jn
+    return (e * Dn * np0) / Ln
 
 
-def count_Js(me: float, mh: float, t: float, Nd: float, Na: float, Eg: float,
+def count_Js(me: float, mh: float, t: float, Eg: float,
              Dn: float, Ln: float, Dp: float, Lp: float, Ef_n: float, Ef_p: float) -> Current:
 
-    Jn = _count_Jn(Nd=Nd, t=t, Dn=Dn, Ln=Ln, me=me, mh=mh, Eg=Eg, Ef_p=Ef_p)
-    Jp = _count_Jp(Na=Na, t=t, Dp=Dp, Lp=Lp, me=me, mh=mh, Eg=Eg, Ef_n=Ef_n)
+    Jn = _count_Jn(t=t, Dn=Dn, Ln=Ln, me=me, mh=mh, Eg=Eg, Ef_p=Ef_p)
+    Jp = _count_Jp(t=t, Dp=Dp, Lp=Lp, me=me, mh=mh, Eg=Eg, Ef_n=Ef_n)
     Js = Jn + Jp
     return Current(js=Js, jn=Jn, jp=Jp)
 
@@ -91,7 +105,6 @@ def calclate_I_amper(js: float, s: float) -> float:
 
 
 def volt_amper_characteristic(js: float, s: float, t: float, path: str):
-    e = 1.6e-19  # Кулон
     k = 1.38e-23  # J/K
     u = np.linspace(-1, 1, 1000)
     J = []
