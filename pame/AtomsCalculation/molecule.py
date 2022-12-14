e161e8725da31f750f759d1102139a2ca8e0234f
Kaseke <kasymkhankhubievnis@gmail.com>
2022-12-14 10:27:55 +0700

    small fixes

--- pame/AtomsCalculation/molecule.py
+++ pame/AtomsCalculation/molecule.py
@@ -7,9 +7,9 @@ class Molecule:
         self.a, self.m_a, self.v = a, m_a, a**3
 
     def atoms_density(self) -> float:
-        n_a = 6.022e23
+        n_a = 6.022e23  # 1/mole
         n = 8
-        rho = self.m_a * n / (n_a * self.v)
+        rho = self.m_a * n / (n_a * self.v)  # g/mole / (1/mole * cm^3) = g/cm^3
         return rho
 
     def atoms_number(self) -> float:
